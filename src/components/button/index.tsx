6462bd2c97a3129aad2278a785b217883395b0a9
LucasLevingston <lucaolevingston@outlook.com>
2024-12-12 13:45:59 -0300

    first commit

---
+++ src/components/button/index.tsx
@@ -0,0 +1,7 @@
+import { TouchableOpacity, TouchableOpacityProps } from 'react-native'
+
+import { s } from './styles'
+
+function Button() {
+  return <TouchableOpacity></TouchableOpacity>
+}
---
+++ src/components/button/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native'
+import { colors, fontFamily } from '@/styles/theme'
+
+export const s = StyleSheet.create({
+  container: {},
+  title: {
+    fontSize: 24,
+    fontFamily: fontFamily.bold,
+    color: colors.gray[600],
+  },
+})
