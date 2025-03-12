3df4e37c874fd158ad992a1923c8f17141565b61
LucasLevingston <lucaolevingston@outlook.com>
2025-03-12 14:54:33 -0300

    api and pictures

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,6 +1,6 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://192.168.0.12:3333',
+  baseURL: 'http://192.168.0.22:3333',
   timeout: 700,
-})
+});
